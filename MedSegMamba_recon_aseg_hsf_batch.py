ea4dabe327e8946725230fe252c3163f33562f72
Aaron Cao <aaroncao06@gmail.com>
2025-04-24 23:54:15 -0400

    fix inference script

--- MedSegMamba_recon_aseg_hsf_batch.py
+++ MedSegMamba_recon_aseg_hsf_batch.py
@@ -1,9 +1,3 @@
-#32577
-#nohup /home/raphael/anaconda3/envs/SAIL/bin/python /media/sail/HDD18T/BME_Grad_Project/Aaron_DiffSurfer/medsegmamba_hsf/MedSegMamba_recon_HSF_batch.py  > /media/sail/HDD18T/BME_Grad_Project/Aaron_DiffSurfer/medsegmamba_hsf/run_on_Step2_3DDeepC_T1_MNI152affine_iso1mm.9.25.24.out &
-
-#4288
-#nohup /home/raphael/anaconda3/envs/SAIL/bin/python /media/sail/HDD18T/BME_Grad_Project/Aaron_DiffSurfer/medsegmamba_hsf/MedSegMamba_recon_HSF_batch.py  > /media/sail/HDD18T/BME_Grad_Project/Aaron_DiffSurfer/medsegmamba_hsf/run_on_Step4_3DDeepC_T1_MNI152affine_iso1mm_WM_Lin_Normalized.10.7.24.out &
-
 import subprocess
 import os
 import pathlib
@@ -30,10 +24,11 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
-parser.add_argument('--no_conform', default=True, type=str2bool)
+    
+parser.add_argument('--no_conform', default=False, type=str2bool)
 
-parser.add_argument('--aseg_model_path', default='', type=str)
-parser.add_argument('--hsf_model_path', default='', type=str)
+parser.add_argument('--aseg_model_path', default=None, type=str)
+parser.add_argument('--hsf_model_path', default=None, type=str)
 
 parser.add_argument('--python_env_path', default=None, type=str) 
 
@@ -98,15 +93,15 @@ if __name__ == '__main__':
             if output_hsf_dir is not None:    
                 if os.path.exists(output_hsf_dir):
                     output_hsf_path = os.path.join(output_hsf_dir, f'hsf.{file_name}.nii.gz')
-                    new_args = new_args+['--output_aseg_path', output_hsf_path]
+                    new_args = new_args+['--output_hsf_path', output_hsf_path]
                 else:
                     print('invalid output hsf folder:', output_hsf_dir, flush=True)
 
 
             # Run the sub_script.py with arguments using subprocess
             if args.python_env_path is None:
-                subprocess.run(["python", os.path.join(os.path.dirname(os.path.abspath(__file__)),"MedSegMamba_recon_HSF.py")] + new_args)
+                subprocess.run(["python", os.path.join(os.path.dirname(os.path.abspath(__file__)),"MedSegMamba_recon_aseg_hsf.py")] + new_args)
             else:
-                subprocess.run([args.python_env_path, os.path.join(os.path.dirname(os.path.abspath(__file__)),"MedSegMamba_recon_HSF.py")] + new_args)
+                subprocess.run([args.python_env_path, os.path.join(os.path.dirname(os.path.abspath(__file__)),"MedSegMamba_recon_aseg_hsf.py")] + new_args)
 
     print('done', flush = True)
